9b8611c3552a670f4df5a3edb05b0a42b617ff88
David Ebbo <david.ebbo@gmail.com>
2016-11-17 17:51:57 -0800

    Version 1.3.1

--- KuduSync.NET/Properties/AssemblyInfo.cs
+++ KuduSync.NET/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.1.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("85be3890-b809-4a60-bd5f-5ac30af0d793")]
