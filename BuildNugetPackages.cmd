83e761fd744a3c0f256dbd1829e8aeb9e69fea29
Mattias Karlsson <matkar@wcom.se>
2020-07-08 11:10:18 +0200

    Create NuGet packages on GitHub Actions * fixes #26

--- BuildNugetPackages.cmd
+++ BuildNugetPackages.cmd
@@ -6,5 +6,5 @@ set corefx="netcoreapp3.1"
 set project=".\KuduSync.NET\KuduSync.NET.csproj"
 set output=".\artifacts"
 
-dotnet pack /p:TargetFramework=%corefx% /p:TargetFrameworks=%corefx% %project% --output "%output%"
-dotnet pack /p:TargetFramework=%netfx% /p:TargetFrameworks=%netfx% %project% --output "%output%"
+dotnet pack /p:Configuration=Release /p:TargetFramework=%corefx% /p:TargetFrameworks=%corefx% %project% --output "%output%"
+dotnet pack /p:Configuration=Release /p:TargetFramework=%netfx% /p:TargetFrameworks=%netfx% %project% --output "%output%"
