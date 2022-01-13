eed44916837cb7e17ebf8ecbc5be11b2ed4c1d53
Mattias Karlsson <matkar@wcom.se>
2022-01-13 22:46:02 +0100

    Enable Deterministic Builds

--- BuildNugetPackages.cmd
+++ BuildNugetPackages.cmd
@@ -2,9 +2,8 @@
 setlocal
 
 set netfx="net45"
-set corefx="netcoreapp3.1"
 set project=".\KuduSync.NET\KuduSync.NET.csproj"
 set output=".\artifacts"
 
-dotnet pack /p:Configuration=Release /p:TargetFramework=%corefx% /p:TargetFrameworks=%corefx% %project% --output "%output%"
-dotnet pack /p:Configuration=Release /p:TargetFramework=%netfx% /p:TargetFrameworks=%netfx% %project% --output "%output%"
+dotnet pack /p:Configuration=Release /p:NetFx=false %project% --output "%output%"
+dotnet pack /p:Configuration=Release /p:NetFx=true /p:TargetFramework=%netfx% /p:TargetFrameworks=%netfx% %project% --output "%output%"
